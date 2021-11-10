0d0d9e85d48c67cb4c04b5c546495b27fc962df4
Michele Comolli <mikicomolli@gmail.com>
2021-11-10 11:22:46 +0100

    added review model + actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants do
+    resources :reviews, only: [:new, :create]
     collection do
       get :top
     end
@@ -8,4 +9,6 @@ Rails.application.routes.draw do
       get :chef
     end
   end
+  resources :reviews, only: [:destroy]
+  # delete 'reviews/:id', to: 'reviews#destroy'
 end
